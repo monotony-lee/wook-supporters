abef9c93d4a2991bdcb17ccf3cec76a31a522d92
inseung <nzzang100@hanmail.net>
2021-12-28 13:22:18 +0900

    first commit

---
+++ src/pages/PostListPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import PostListContainer from '../containers/posts/PostListContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
+
+const PostListPage = () => {
+  return (
+    <>
+      <HeaderContainer />
+      <PostListContainer />
+      <PaginationContainer />
+    </>
+  );
+};
+
+export default PostListPage;
+//현재 cookie token도 갖고 있고, localStorage로 가지고 있오
