b092d730a353b19c5485c8541dce383c02427a8f
yhs <hansprac@gmail.com>
2021-12-28 17:43:01 +0900

    mongoose con rev

--- src/main.js
+++ src/main.js
@@ -13,8 +13,10 @@ import jwtMiddleware from './lib/jwtMiddleware';
 // 비구조화 할당을 통하여 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
 
+// { useNewUrlParser: true, useFindAndModify: false }
+
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .connect(MONGO_URI, { useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
